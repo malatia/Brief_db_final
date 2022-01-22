f4275d388f7e0aeeac387e5efe0f743482830d5d
malatia <respeghi@gmail.com>
2022-01-22 19:56:53 +0100

    Update queries.py

--- app/queries.py
+++ app/queries.py
@@ -16,93 +16,28 @@ def one_variable(variable):
         [List]: [Une liste contenant les noms de la catégorie demandée]
     """
 
-    #il y aura beaucoup de code répété, je ne commenterai donc que le premier bloc, les autres étant quasiments identiques
-    if variable == "Job":
-        #On récupère les données dont on va avoir besoin pour créer le grahpique et afficher les noms. On fait une aggrégation dans la deuxieme requete
-        with engine.connect() as conn:
-            result = conn.execute(text("SELECT job.job_name FROM job"))
-            result2 = conn.execute(text("SELECT count(user.user_id), job.job_name  FROM job  JOIN user on user.job_id = job.job_id GROUP BY job.job_name;"))
+    #On récupère les données dont on va avoir besoin pour créer le grahpique et afficher les noms. On fait une aggrégation dans la deuxieme requete
+    variable = variable.lower()
+    with engine.connect() as conn:
+        result = conn.execute(text(f"SELECT {variable}.{variable}_name as name FROM {variable}"))
+        result2 = conn.execute(text(f"SELECT count(user.user_id), {variable}.{variable}_name as name  FROM {variable}  JOIN user on user.{variable}_id = {variable}.{variable}_id GROUP BY {variable}.{variable}_name;"))
 
-        #On récupère les différentes lignes renvoyées dans une liste 
-        name_list = [row.job_name for row in result]
-        data = [row for row in result2]
+    #On récupère les différentes lignes renvoyées dans une liste 
+    name_list = [row.name for row in result]
+    data = [row for row in result2]
 
-        #Ce dataframe est là pour faciliter l'affichage des données 
-        job_df = pd.DataFrame(data, columns=["count", "job"])
-        #On configure ce qu'il faut pour pouvoir créer un graphique à partir des données 
-        plt.rcParams['figure.figsize']=[16,8]
-        job_arr = np.array(job_df["count"])
-        job_labels = job_df["job"].values.tolist()
-        plt.pie(job_arr, labels = job_labels, autopct = '%1.2f%%')
-        plt.legend(title = "Proportion des jobs")
-        #On enregistre la figure à un chemin générique que l'on affiche ensuite dans le HTML
-        plt.savefig("app/static/fig.png")
-        plt.close()
-
-    elif variable == "City":
-        with engine.connect() as conn:
-            result = conn.execute(text("SELECT city.city_name FROM city"))
-            result2 = conn.execute(text("SELECT count(user.user_id), city.city_name  FROM city  JOIN user on user.city_id = city.city_id GROUP BY city.city_name;"))
-
-        name_list = [row.city_name for row in result]
-        data = [row for row in result2]
-        city_df = pd.DataFrame(data, columns=["count", "city"])
-        plt.rcParams['figure.figsize']=[16,8]
-        city_arr = np.array(city_df["count"])
-        city_labels = city_df["city"].values.tolist()
-        plt.pie(city_arr, labels = city_labels, autopct = '%1.2f%%')
-        plt.legend(title = "Proportion des cities")
-        plt.savefig("app/static/fig.png")
-        plt.close()
-
-    elif variable == "Movie":
-        with engine.connect() as conn:
-            result = conn.execute(text("SELECT movie.movie_name FROM movie"))
-            result2 = conn.execute(text("SELECT count(user.user_id), movie.movie_name  FROM movie  JOIN user on user.movie_id = movie.movie_id GROUP BY movie.movie_name;"))
-
-        name_list = [row.movie_name for row in result]
-        data = [row for row in result2]
-        movie_df = pd.DataFrame(data, columns=["count", "movie"])
-        plt.rcParams['figure.figsize']=[16,8]
-        movie_arr = np.array(movie_df["count"])
-        movie_labels = movie_df["movie"].values.tolist()
-        plt.pie(movie_arr, labels = movie_labels, autopct = '%1.2f%%')
-        plt.legend(title = "Proportion des movies")
-        plt.savefig("app/static/fig.png")
-        plt.close()
-
-    elif variable == "Country":
-        with engine.connect() as conn:
-            result = conn.execute(text("SELECT country.country_name FROM country"))
-            result2 = conn.execute(text("SELECT count(user.user_id), country.country_name  FROM country  JOIN user on user.country_id = country.country_id GROUP BY country.country_name;"))
-
-        name_list = [row.country_name for row in result]
-        data = [row for row in result2]
-        country_df = pd.DataFrame(data, columns=["count", "country"])
-        plt.rcParams['figure.figsize']=[16,8]
-        country_arr = np.array(country_df["count"])
-        country_labels = country_df["country"].values.tolist()
-        plt.pie(country_arr, labels = country_labels, autopct = '%1.2f%%')
-        plt.legend(title = "Proportion des countries")
-        plt.savefig("app/static/fig.png")
-        plt.close()
-
-    elif variable == "Domain":
-        with engine.connect() as conn:
-            result = conn.execute(text("SELECT domain.domain_name FROM domain"))
-            result2 = conn.execute(text("SELECT count(user.user_id), domain.domain_name  FROM domain  JOIN user on user.domain_id = domain.domain_id GROUP BY domain.domain_name;"))
-        
-        name_list = [row.domain_name for row in result]        
-        data = [row for row in result2]
-        domain_df = pd.DataFrame(data, columns=["count", "domain"])
-        plt.rcParams['figure.figsize']=[16,8]
-        domain_arr = np.array(domain_df["count"])
-        domain_labels = domain_df["domain"].values.tolist() 
-        plt.pie(domain_arr, labels = domain_labels, autopct = '%1.2f%%')
-        plt.legend(title = "Proportion des domains")
-        plt.savefig("app/static/fig.png")
-        plt.close()
-
+    #Ce dataframe est là pour faciliter l'affichage des données 
+    df = pd.DataFrame(data, columns=["count", "job"])
+    #On configure ce qu'il faut pour pouvoir créer un graphique à partir des données 
+    plt.rcParams['figure.figsize']=[16,8]
+    arr = np.array(df["count"])
+    labels = df["job"].values.tolist()
+    plt.pie(arr, labels = labels, autopct = '%1.2f%%')
+    plt.legend(title = "Proportion des jobs")
+    #On enregistre la figure à un chemin générique que l'on affiche ensuite dans le HTML
+    plt.savefig("app/static/fig.png")
+    plt.close()
+    
     return name_list
 
 def two_variables(variable1, variable2):
